39e4919967e24cdb65c19fc7a7be7fc9a39a6bdb
unknown <x15g001@PC504N012.chiba-fjb.ac.jp>
2015-11-02 14:40:10 +0900

    2

---
+++ src/jp/ac/chibafjb/x15g001/kd102/wa.java
@@ -0,0 +1,17 @@
+package jp.ac.chibafjb.x15g001.kd102;
+
+public class wa {
+	public static void main(String[] args) {
+		System.out.println("船橋　太郎");
+		System.out.println("船橋　太郎");
+		System.out.println("船橋　太郎");
+		System.out.println("船橋　太郎");
+		System.out.println("船橋　太郎");
+		System.out.println("船橋　太郎");
+		System.out.println("船橋　太郎");
+		System.out.println("船橋　太郎");
+		System.out.println("船橋　太郎");
+		System.out.println("船橋　太郎");
+		System.out.println("船橋　太郎");
+	}
+}
